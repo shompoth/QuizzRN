bed70739b677a395131f3cf350894203fdb5b5be
Shompoth KHAN <dadoulan@gmail.com>
2024-12-11 13:36:50 +0100

    Create component wrapper card

---
+++ src/components/Card.tsx
@@ -0,0 +1,42 @@
+import { StyleSheet, Text, View } from "react-native";
+
+type Card = {
+  title: string;
+  children: React.ReactNode;
+};
+
+const Card = ({ title, children }: Card) => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>{title}</Text>
+      {children}
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "white",
+    padding: 20,
+    paddingVertical: 40,
+    borderRadius: 20,
+    gap: 20,
+
+    // shadows
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+
+    elevation: 5,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 500,
+  },
+});
+
+export default Card;
--- src/components/QuestionCard.tsx
+++ src/components/QuestionCard.tsx
@@ -1,12 +1,14 @@
-import { Text, View, StyleSheet } from "react-native";
+import { PropsWithChildren } from "react";
+import { View } from "react-native";
 import AnswerOption from "./AnswerOption";
 import { Question } from "../types";
+import Card from "./Card";
 
 type QuestionCard = {
   question: Question;
 };
 
-const QuestionCard = ({ question }: QuestionCard) => {
+const QuestionCard = ({ question }: PropsWithChildren<QuestionCard>) => {
   const selectedOption = question.options[0];
 
   const onOptionSelected = (option: string) => {
@@ -14,9 +16,8 @@ const QuestionCard = ({ question }: QuestionCard) => {
   };
 
   return (
-    <View style={styles.questionCard}>
-      <Text style={styles.question}>{question.title}</Text>
-      <View style={styles.answerOptionContainer}>
+    <Card title={question.title}>
+      <View style={{ gap: 10 }}>
         {question.options.map((option) => (
           <AnswerOption
             key={option}
@@ -26,36 +27,8 @@ const QuestionCard = ({ question }: QuestionCard) => {
           />
         ))}
       </View>
-    </View>
+    </Card>
   );
 };
-
-const styles = StyleSheet.create({
-  questionCard: {
-    backgroundColor: "white",
-    padding: 20,
-    paddingVertical: 40,
-    borderRadius: 20,
-    gap: 20,
-
-    // shadows
-    shadowColor: "#000",
-    shadowOffset: {
-      width: 0,
-      height: 2,
-    },
-    shadowOpacity: 0.25,
-    shadowRadius: 3.84,
-
-    elevation: 5,
-  },
-  question: {
-    fontSize: 24,
-    fontWeight: 500,
-  },
-  answerOptionContainer: {
-    gap: 10,
-  },
-});
 
 export default QuestionCard;
